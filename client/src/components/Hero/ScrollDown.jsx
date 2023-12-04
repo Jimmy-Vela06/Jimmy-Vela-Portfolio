b91f52b39b5a605f46528044dabfb4a8fac9aa03
Jimmy Vela <jimmyvela06@.com>
2023-12-04 04:37:41 -0600

    added about me

--- client/src/components/Hero/ScrollDown.jsx
+++ client/src/components/Hero/ScrollDown.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const ScrollDown = () => {
   return (
     <div className='scrollHero'>
-      <button href='#about' className='scrollBtn'>
+      <a href='#aboutMe' className='scrollBtn'>
         <span className='hero__scroll-name'>Scroll Down</span> &nbsp;
         <i class='uil uil-arrow-down home__scroll-arrow'></i>
-      </button>
+      </a>
     </div>
   );
 };
